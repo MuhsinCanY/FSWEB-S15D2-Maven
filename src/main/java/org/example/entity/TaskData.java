e7a56a17f32528da41ed5040b76ad1dcbb8aea8d
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-04-02 17:29:33 +0300

    S15D2 Copmleted

---
+++ src/main/java/org/example/entity/TaskData.java
@@ -0,0 +1,57 @@
+package org.example.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaskData {
+
+    private Set<Task> annsTasks;
+    private Set<Task> bobsTasks;
+    private Set<Task> carolsTasks;
+    private Set<Task> unassignedTasks;
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks,
+                    Set<Task> carolsTasks, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    public Set<Task> getTasks(String name) {
+        switch (name) {
+            case ("ann"):
+                return this.annsTasks;
+            case ("bob"):
+                return this.bobsTasks;
+            case ("carol"):
+                return this.carolsTasks;
+            case ("all"):
+                return getUnion(this.unassignedTasks,this.annsTasks,
+                                this.bobsTasks,this.carolsTasks);
+            default:
+                return null;
+        }
+
+    }
+
+    public Set<Task> getUnion(Set<Task> ...sets){
+        Set<Task> unionTasks = new HashSet<>();
+
+        for (Set<Task> set: sets){
+            unionTasks.addAll(set);
+        }
+        return unionTasks;
+    }
+
+    public Set<Task> getIntersection(Set<Task> set1, Set<Task> set2){
+        set1.retainAll(set2);
+        return set1;
+    }
+
+    public Set<Task> getDifferences(Set<Task> set1, Set<Task> set2){
+        set1.removeAll(set2);
+        return set1;
+    }
+
+}
